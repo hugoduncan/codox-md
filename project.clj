3882bd6cdeb06f5aaf3d9ac07746b50abba34a86
Hugo Duncan <hugo@hugoduncan.org>
2012-02-12 22:48:10 -0500

    Update to codox 0.4.0

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject codox-md "0.1.0-SNAPSHOT"
   :description "Templated codox output via markdown"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+  :dependencies [[org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [enlive "1.0.0"]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0-lc"]
-                     [codox "0.3.5-SNAPSHOT"]]
+  :dev-dependencies [[codox "0.4.0"]]
   :codox {:writer codox-md.writer/write-docs})
