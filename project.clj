5bdaac9037fb2e8afbbf92f5c4ad38440b24ec31
Hugo Duncan <hugo@hugoduncan.org>
2013-01-20 22:40:44 -0500

    Update version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject codox-md "0.1.0"
+(defproject codox-md "0.1.1-SNAPSHOT"
   :description "Templated codox output with markdown processing."
   :dependencies [[org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [enlive "1.0.0"]]
